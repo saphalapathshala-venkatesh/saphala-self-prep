e3ab8e887d02f0040260dc7f2a0dc193f5151347
saphalapathshala-venkatesh <saphalapathshala555@gmail.com>
2026-03-20 12:07:36 +0000

    Add TGPSC category banner image

--- web/config/categoryImages.ts
+++ web/config/categoryImages.ts
@@ -7,6 +7,7 @@ export const CATEGORY_IMAGE_MAP: Record<string, string> = {
   cmmruwqhz000395x32wjb9s2w: "/images/categories/ap-police.png", // AP Police
   cmmruwcuh000095x3ad4aah4g: "/images/categories/appsc.png", // APPSC
   cmmrux7lm000995x3btm3vww2: "/images/categories/tg-police.png", // TG Police
+  cmmruwy7d000695x3ryhxpb0k: "/images/categories/tgpsc.png", // TGPSC
 };
 
 export function getCategoryImage(categoryId: string): string | null {
