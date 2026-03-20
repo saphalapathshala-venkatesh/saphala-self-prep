088a7bd78b23d68577c7dba55af9bbbcbc7963b4
saphalapathshala-venkatesh <saphalapathshala555@gmail.com>
2026-03-20 12:12:55 +0000

    Add UPSC category banner image

--- web/config/categoryImages.ts
+++ web/config/categoryImages.ts
@@ -8,6 +8,7 @@ export const CATEGORY_IMAGE_MAP: Record<string, string> = {
   cmmruwcuh000095x3ad4aah4g: "/images/categories/appsc.png", // APPSC
   cmmrux7lm000995x3btm3vww2: "/images/categories/tg-police.png", // TG Police
   cmmruwy7d000695x3ryhxpb0k: "/images/categories/tgpsc.png", // TGPSC
+  cmmruxg8n000c95x3y8typk5h: "/images/categories/upsc.png", // UPSC
 };
 
 export function getCategoryImage(categoryId: string): string | null {
